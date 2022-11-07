8be6d33fa3f1202ec1eae8e8bd26e14689840231
Matusita <matusitapedro@gmail.com>
2022-11-07 10:13:59 -0300

    Pré-requisitos (Matusita)

---
+++ Fortuna_Dessoft.py
@@ -0,0 +1,19 @@
+#pré-requisitos
+
+
+#condições iniciais
+lista_premios = [0,1000,5000,10000,30000,50000,100000,300000,500000,1000000]
+dinheiro = lista_premios[0]
+pulo = 3
+ajuda = 2
+
+nome = input('INFORME SEU NOME: ')
+
+print('\nBEM-VINDO A FORTUNA DESSOFT {0}'.format(nome.upper()))
+
+#manual
+print('\n-----------------\nMANUAL\n-----------------\n')
+print('Cada pergunta tem quatro alternativas: "A" , "B" , "C" , "D"\nVocê também tem direito de "ajuda" , "pula" , "parar"')
+
+
+print('\nAgora, que você conhece as regras:\n\nQUE COMECE A FORTUNA DESSOFT!!')
